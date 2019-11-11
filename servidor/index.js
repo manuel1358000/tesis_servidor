bfb8e1663495d15db78a87cacbe45d1ab0beada0
manuel1358000 <manuel13580@gmail.com>
2019-11-10 21:51:41 -0600

    Se agregaron las rutas a consumir para las publicaciones

--- servidor/index.js
+++ servidor/index.js
@@ -8,23 +8,46 @@ app.use(express.static('src'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 conexionbd.client.connect();
+app.post('/post/iniciar_sesion',function(req,res){
+    console.log(req.body);
+    inicioSesion(req.body.CUI,req.body.PASSWORD).then((respuesta)=>{
+        res.json(respuesta);
+    });
+});
 app.post('/post/usuarioAU',function(req,res){
     registrarUsuarios(req.body.CUI,req.body.NOMBRE,req.body.PASSWORD,req.body.TIPO,req.body.ESTADO).then((respuesta)=>{
             res.json(respuesta);
     });
 });
+app.get('/get/usuarioAU',function(req,res){
+    //si tiene algun parametro va a buscar un usuario en especifico
+    //si no lo tiene regresa a todos los usuarios
+}); 
 
-app.post('/post/iniciar_sesion',function(req,res){
-    inicioSesion(req.body.CUI,req.body.PASSWORD).then((respuesta)=>{
-        res.json(respuesta);
-    });
+app.put('/put/usuarioAU',function(req,res){
+    //actualizacion de un usuario
 });
-
 app.delete('/delete/usuarioAU',function(req,res){
     eliminarUsuario(req.body.CUI).then((respuesta)=>{
         res.json(respuesta);
     });
 });
+
+app.post('/post/publicacionAU',function(req,res){
+    
+});
+app.get('/get/publicacionAU',function(req,res){
+    //si tiene algun parametro va a buscar un usuario en especifico
+    //si no lo tiene regresa a todos los usuarios
+}); 
+
+app.put('/put/publicacionAU',function(req,res){
+    //actualizacion de un usuario
+});
+app.delete('/delete/publicacionAU',function(req,res){
+    
+});
+
 
 async function eliminarUsuario(cui){
     var query="delete from usuario where cui="+cui;
@@ -33,6 +56,7 @@ async function eliminarUsuario(cui){
         if(res.rowCount==1) return {"codigo":200,"mensaje":"Usuario eliminado con exito"};
         return {"codigo":200,"mensaje":"No existe un usuario con el CUI que quiere eliminar"};
     }).catch(e=>{
+        console.log(e);
         return {"codigo":501,"mensaje":"Error al momento de eliminar al usuario, intente nuevamente"};
     });    
     return respuesta;
@@ -52,11 +76,13 @@ async function registrarUsuarios(cui,nombre,password,tipo,estado){
 async function inicioSesion(cui,password){
     if(cui==''&&password=='')return {"codigo":402,"mensaje":"Datos incompletos al iniciar sesion, intente nuevamente"};
     var query="select cod_usuario,nombre,tipo,estado from usuario where cui="+cui+" and password='"+password+"'";
+    console.log(query);
     const respuesta=await conexionbd.client.query(query)
     .then(res=>{
         if(res.rowCount==1) return {"codigo":200,"mensaje":"Inicio de Sesion Exitoso","data":res.rows[0]};
         return {"codigo":200,"mensaje":"Por Favor ingrese un CUI/PASSWORD valido"};
     }).catch(e=>{
+        console.log(e);
         return {"codigo":501,"mensaje":"Error al momento de iniciar sesion, intente nuevamente"};
     })
     return respuesta;
