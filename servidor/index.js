197f272aaf8038f1cb4a8335bf448aefd17fab95
manuel1358000 <manuel13580@gmail.com>
2019-11-10 18:24:04 -0600

    Se crearon las operaciones CREATE, DELETE del usuario y se agrego la url de inicio de sesion

--- servidor/index.js
+++ servidor/index.js
@@ -8,71 +8,59 @@ app.use(express.static('src'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 conexionbd.client.connect();
-app.post('/auth', function (req, res) {
-    var url=req.body.url;
-    var rol=req.body.rol;
-    if((url!=null||rol!='')||(rol!=null)){
-        if(rol=='INVITADO'){
-            var payload={
-                auth:true,
-                idusuario:'0',
-                rol:rol
-            };
-            var token=generarToken(payload);
-            res.send('{"codigo":"200","mensaje":"OK","data":{"token":"'+token+'","nombre":"INVITADO","url":""}}');
-        }else{
-            var idusuario='123456001';
-            var carne='201213580';
-            var nombre='Manuel Fuentes';
-            var cod_respuesta='';  
-            if(idusuario!=''){
-                verificarInformacion(idusuario)
-                .then(function(verificar){
-                    if(verificar){
-                        var payload={
-                            auth:true,
-                            idusuario:idusuario,
-                            rol:rol
-                        };
-                        var token=generarToken(payload);
-                        console.log('Cod: 200 Mensaje: Usuario Existente');
-                        res.send('{"codigo":"200","mensaje":"OK","data":{"token":"'+token+'","nombre":"'+nombre+'","url":"'+url+'"}}');
-                    }else{
-                        registrarUsuarios(idusuario,carne,nombre,rol)
-                        .then(function(respuesta){
-                            if(respuesta){
-                                var payload={
-                                    auth:true,
-                                    idusuario:idusuario,
-                                    rol:rol
-                                };
-                                console.log('Cod: 200 Mensaje: Usuario Registrado');
-                                var token=generarToken(payload);
-                                res.send('{"codigo":"200","mensaje":"OK","data":{"token":"'+token+'","nombre":"'+nombre+'","url":"'+url+'"}}');
-                            }else{
-                                console.log('Cod: 500 Mensaje: Error al registrar usuario');
-                                cod_respuesta='{"codigo":"400","mensaje":"Error al registrar usuarios"}';
-                                res.send(cod_respuesta);
-                            }
-                        });
-                    }
-                });
-            }else{
-                cod_respuesta='{"codigo":"400","mensaje":"Parametros null"}';
-                res.send(cod_respuesta);
-            }
-        }
-    }else{
-        cod_respuesta='{"codigo":"400","mensaje":"Parametros null"}';
-        res.send(cod_respuesta);
-    }
+app.post('/post/usuarioAU',function(req,res){
+    registrarUsuarios(req.body.CUI,req.body.NOMBRE,req.body.PASSWORD,req.body.TIPO,req.body.ESTADO).then((respuesta)=>{
+            res.json(respuesta);
+    });
 });
 
-app.get('/prueba',function(req,res){
-    var cod_respuesta='{"codigo":"400","mensaje":"Parametros null"}';
-    res.send(cod_respuesta);
+app.post('/post/iniciar_sesion',function(req,res){
+    inicioSesion(req.body.CUI,req.body.PASSWORD).then((respuesta)=>{
+        res.json(respuesta);
+    });
 });
 
+app.delete('/delete/usuarioAU',function(req,res){
+    eliminarUsuario(req.body.CUI).then((respuesta)=>{
+        res.json(respuesta);
+    });
+});
+
+async function eliminarUsuario(cui){
+    var query="delete from usuario where cui="+cui;
+    const respuesta=await conexionbd.client.query(query)
+    .then(res=>{
+        if(res.rowCount==1) return {"codigo":200,"mensaje":"Usuario eliminado con exito"};
+        return {"codigo":200,"mensaje":"No existe un usuario con el CUI que quiere eliminar"};
+    }).catch(e=>{
+        return {"codigo":501,"mensaje":"Error al momento de eliminar al usuario, intente nuevamente"};
+    });    
+    return respuesta;
+}
+async function registrarUsuarios(cui,nombre,password,tipo,estado){
+    var query="insert into usuario(cui,nombre,password,tipo,estado)values("+cui+",'"+nombre+"','"+password+"',"+tipo+","+estado+")";
+    const respuesta=await conexionbd.client.query(query)
+    .then(res=>{
+        return {"codigo":200 ,"mensaje":"Usuario Creado con Exito"};
+    }).catch(e=>{
+        if (e.code==23505) return {"codigo":200,"mensaje":"Ya existe un usuario registrado el numero de CUI ingresado"};
+            return {"codigo":501,"mensaje":"Error al momento de registrar al usuario, intente nuevamente"};
+        });    
+    return respuesta;
+}
+
+async function inicioSesion(cui,password){
+    if(cui==''&&password=='')return {"codigo":402,"mensaje":"Datos incompletos al iniciar sesion, intente nuevamente"};
+    var query="select cod_usuario,nombre,tipo,estado from usuario where cui="+cui+" and password='"+password+"'";
+    const respuesta=await conexionbd.client.query(query)
+    .then(res=>{
+        if(res.rowCount==1) return {"codigo":200,"mensaje":"Inicio de Sesion Exitoso","data":res.rows[0]};
+        return {"codigo":200,"mensaje":"Por Favor ingrese un CUI/PASSWORD valido"};
+    }).catch(e=>{
+        return {"codigo":501,"mensaje":"Error al momento de iniciar sesion, intente nuevamente"};
+    })
+    return respuesta;
+}
 
 app.post('/verificartoken',function(req,res){
     generador_jwt.verificarToken(req.body.token)
@@ -86,41 +74,6 @@ function generarToken(payload){
     return token;
 }
 
-async function registrarUsuarios(idusuario,carne,nombre,rol){
-    if((carne==null||carne==null)||(nombre==null||nombre==null)){
-        console.log(rol);
-        return false;
-    }else{
-        if(rol=='ADMINISTRADOR'||rol=='USUARIO'){
-            var query="insert into usuario(idusuario,carne,nombre,rol)values('"+idusuario+"','"+carne+"','"+nombre+"','"+rol+"')";
-            const res=await conexionbd.client.query(query)
-            .then(res=>{
-                console.log(res);
-                return true;
-            }).catch(e=>{
-                console.log('Cod: 501 Mensaje: '+e);
-                return false;
-            });
-            return res;
-        }else{
-            return false;
-        }
-    }
-}
-
-async function verificarInformacion(idusuario){
-    const res=await conexionbd.client.query('select * from usuario where idusuario=$1',[idusuario])
-    .then(res=>{
-        if(res.rows.length==1){
-            return true;
-        }
-        return false;
-    }).catch(e=>{
-        console.log('Cod: 501 Mensaje: '+e);
-        return false;
-    });
-    return res;
-}
 app.listen(8080,'0.0.0.0', () => {
     console.log("Servidor APP AlertaUSAC en la direccion 0.0.0.0");
 });
