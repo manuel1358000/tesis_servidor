06e90eff5eb6d8d75336e11d95b10dcfc2f7d4bd
manuel1358000 <manuel13580@gmail.com>
2020-12-23 20:59:26 -0600

    Commit central

---
+++ datosPreliminares.sql
@@ -0,0 +1,83 @@
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('1','Alerta Medica','Alerta medica entrada principal CUM',14.611534,-90.5452724,1,1,'2019-11-20 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('1','Accidente','Accidente Entrada Cum, dos vehiculos, entrada del parqueo no esta habilitada',14.611522,-90.5452737,1,1,'2019-11-11 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('1','Robo','Robo celular afueras del cum, persona con camisa amarilla y pantalon de lona negro',14.611513,-90.5452737,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('2','Conferencia Medica','Conferencia medica alumnos primer año medicina seccion A, tema central de la conferencia cardiologia y medicina interna.',14.611519,-90.5452737,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('2','Conferencia','Conferencia medica alumnos primer año medicina seccion A, tema central de la conferencia cardiologia y medicina interna.',14.611525,-90.5452737,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('2','Platica Informativa','Platica informativa alumnos primer año medicina seccion A, tema central de la conferencia cardiologia y medicina interna.',14.611524,-90.5452737,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('2','Platica Informativa','Platica informativa alumnos primer año medicina seccion A, tema central de la conferencia cardiologia y medicina interna.',14.611523,-90.5452737,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('2','Foro','Foro alumnos primer año medicina seccion A, tema central de la conferencia cardiologia y medicina interna.',14.611521,-90.5452737,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('2','Foro','Foro alumnos primer año medicina seccion A, tema central de la conferencia cardiologia y medicina interna.',14.611520,-90.5452737,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('2','Debate','Debate alumnos primer año medicina seccion A, tema central de la conferencia cardiologia y medicina interna.',14.611522,-90.5452714,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('2','Debate','Debate alumnos primer año medicina seccion A, tema central de la conferencia cardiologia y medicina interna.',14.611522,-90.5452732,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('2','Presentacion','Presentacion alumnos primer año medicina seccion A, tema central de la conferencia cardiologia y medicina interna.',14.611520,-90.5452746,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('2','Presentacion','Presentacion alumnos primer año medicina seccion A, tema central de la conferencia cardiologia y medicina interna.',14.611522,-90.5452750,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('2','Actividad Cultural','Actividad cultural alumnos primer año medicina seccion A, tema central de la conferencia cardiologia y medicina interna.',14.611530,-90.5452740,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('2','Mesa Dialogo','Mesa dialogo alumnos primer año medicina seccion A, tema central de la conferencia cardiologia y medicina interna.',14.611519,-90.5452739,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('2','Mesa Informativa','Mesa informativa alumnos primer año medicina seccion A, tema central de la conferencia cardiologia y medicina interna.',14.611517,-90.5452736,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('2','Actividad Deportiva','Actividad deportiva alumnos primer año medicina seccion A, tema central de la conferencia cardiologia y medicina interna.',14.611518,-90.5452735,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('2','Actividad Recreativa','Actividad recreativa alumnos primer año medicina seccion A, tema central de la conferencia cardiologia y medicina interna.',14.611514,-90.5452734,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('1','Accidente','Accidente Entrada Cum, dos vehiculos, entrada del parqueo no esta habilitada',14.611505,-90.5452737,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('1','Emergencia','Alerta medica entrada principal CUM',14.611522,-90.5452723,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('1','Robo','Robo celular afueras del cum, persona con camisa amarilla y pantalon de lona negro',14.611520,-90.5452730,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('1','Emergencia','Alerta medica entrada principal CUM',14.611522,-90.5452722,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('1','Emergencia','Alerta medica entrada principal CUM',14.611522,-90.5452730,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('1','Emergencia','Alerta medica entrada principal CUM',14.611522,-90.5452733,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('1','Robo','Robo celular afueras del cum, persona con camisa amarilla y pantalon de lona negro',14.611522,-90.5452737,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('1','Robo','Conferencia media alumnos primer año medicina seccion A, tema central de la conferencia cardiologia y medicina interna.',14.611519,-90.5452731,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('1','Robo','Conferencia media alumnos primer año medicina seccion A, tema central de la conferencia cardiologia y medicina interna.',14.611517,-90.5452730,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('1','Alerta Medica','Alerta medica entrada principal CUM',14.611522,-90.5452734,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('1','Alerta Medica','Alerta medica entrada principal CUM',14.611522,-90.5452738,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('1','Emergencia','Alerta medica entrada principal CUM',14.611512,-90.5452740,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('1','Emergencia','Alerta medica entrada principal CUM',14.611530,-90.5452749,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('1','Emergencia','Alerta medica entrada principal CUM',14.611514,-90.5452745,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('1','Accidente','Accidente Entrada Cum, dos vehiculos, entrada del parqueo no esta habilitada',14.611530,-90.5452729,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('1','Accidente','Accidente Entrada Cum, dos vehiculos, entrada del parqueo no esta habilitada',14.611510,-90.5452724,1,1,'2020-01-09 20:55:00-06');
+insert into publicacion(tipo,nombre,descripcion,posicion_x,posicion_y,estado,subtipo,fechahora)values('1','Accidente','Accidente Entrada Cum, dos vehiculos, entrada del parqueo no esta habilitada',14.611509,-90.5452722,1,1,'2020-01-09 20:55:00-06');
+
+CREATE TABLE public.asignacion (
+    fechahora timestamp with time zone,
+    cod_usuario integer NOT NULL,
+    cod_publicacion integer NOT NULL
+);
+ALTER TABLE public.asignacion OWNER TO postgres;
+ALTER TABLE ONLY public.asignacion
+    ADD CONSTRAINT fk_cod_publicacion FOREIGN KEY (cod_publicacion) REFERENCES public.publicacion(cod_publicacion);
+
+
+--
+-- TOC entry 2692 (class 2606 OID 24706)
+-- Name: asignacion fk_cod_usuario; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.asignacion
+    ADD CONSTRAINT fk_cod_usuario FOREIGN KEY (cod_usuario) REFERENCES public.usuario(cod_usuario);
+
+
+
+
+
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',211,1);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',211,2);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',211,3);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',211,4);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',211,5);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',211,6);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',211,7);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',211,8);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',211,9);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',211,10);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',211,11);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',212,12);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',212,13);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',212,14);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',212,15);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',212,16);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',212,17);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',212,18);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',212,19);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',212,20);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',212,21);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',212,22);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',212,23);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',212,24);
+insert into asignacion(fechahora,cod_usuario,cod_publicacion)values('2020-01-13 20:55:00-06',212,25);
